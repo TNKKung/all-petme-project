4d3051bada51d87ddde1f1cc48e9f8e5c732e8a2
Chatkul Rattanarithikul <77426203+MisterCellar@users.noreply.github.com>
2021-05-05 03:59:13 +0700

    Revert "Merge branch 'mergeBranch' of https://github.com/TNKKung/Project_Web_PetMe into mergeBranch"
    This reverts commit efec56e160db4562cffd9bd9ff853428a286b89c, reversing
    changes made to 34b56cee05afb1a12907339bc7fd23068c88baf0.

---
+++ back-end/test-promptpay/index.js
@@ -0,0 +1,20 @@
+const expressFunction = require("express");
+const expressApp = expressFunction();
+const generatePayload = require('promptpay-qr') 
+const qrcode = require('qrcode') 
+const fs = require('fs') 
+
+const mobileNumber = '0931425175' 
+const amount = 1
+const payload = generatePayload(mobileNumber, { amount }) 
+console.log(payload)
+
+const options = { type: 'svg', color: { dark: '#000', light: '#fff' } }
+qrcode.toString(payload, options, (err, svg) => {
+    if (err) return console.log(err)
+    fs.writeFileSync('./qr.svg', svg)
+    console.log(svg)
+})
+
+
+  
